bfdd10322422f92162b8fa688264fd601224cf9c
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-07-05 18:36:11 -0600

    Placing views with frames.

--- ProgrammaticUI/ViewController.swift
+++ ProgrammaticUI/ViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.setTitle("Button", for: .normal)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setButtonWithFrames()
+    }
 
-        self.view.backgroundColor = .systemBlue
+    // frames is a way of placing views, but it's not the recommended one
+    func setButtonWithFrames() {
+        button.frame = CGRect(x: 100, y: 300,
+                              width: 120, height: 44)
+        self.view.addSubview(button)
     }
 }
 
